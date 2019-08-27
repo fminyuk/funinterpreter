679264d332870992366827520e26b0263ed2d74a
Minyuk Fedor <fminyuk@gmail.com>
2019-08-27 23:56:18 +0300

    remove

--- src/main/scala/org/nnc/funexpr/interpreters/SymbolTableMap.scala
+++
@@ -1,8 +0,0 @@
-package org.nnc.funexpr.interpreters
-
-import org.nnc.funexpr.interpreters.values.Value
-
-class SymbolTableMap(values: Map[String, Value]) extends SymbolTable {
-
-  override def getValue(ident: String): Value = values(ident)
-}
