1ec2ffaaf535f9a4ffcc9d802f5180accae0acec
Minyuk Fedor <fminyuk@gmail.com>
2019-09-03 01:08:59 +0300

    symbol table compose list

--- src/main/scala/org/nnc/funexpr/interpreters/SymbolTableCompose.scala
+++ src/main/scala/org/nnc/funexpr/interpreters/SymbolTableCompose.scala
@@ -2,17 +2,17 @@ package org.nnc.funexpr.interpreters
 
 import scala.annotation.tailrec
 
-class SymbolTableCompose(symbolTables: Seq[SymbolTable]) extends SymbolTable {
+class SymbolTableCompose(symbolTables: List[SymbolTable]) extends SymbolTable {
   override def getValue(ident: String): Option[Value] = {
     getValue(ident, symbolTables.head, symbolTables.tail)
   }
 
   @tailrec
-  private def getValue(ident: String, head: SymbolTable, tail: Seq[SymbolTable]): Option[Value] = {
+  private def getValue(ident: String, head: SymbolTable, tail: List[SymbolTable]): Option[Value] = {
     head.getValue(ident) match {
       case None => tail match {
-        case Seq() => None
-        case h +: t => getValue(ident, h, t)
+        case Nil => None
+        case h :: t => getValue(ident, h, t)
       }
       case value => value
     }
